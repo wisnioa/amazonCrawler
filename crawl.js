4d9ec3ab4d0902ccc74844e7dadad37a110c5cdb
unknown <amanda.wisniowski@gmail.com>
2018-07-12 22:17:47 -0500

    added writeFile function to store scrapes

--- crawl.js
+++ crawl.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+var fs = require("fs");
+
 
 let crawlAmazon = async () => {
     const browser = await puppeteer.launch({ headless: false });
@@ -25,36 +27,47 @@ let crawlAmazon = async () => {
                 let fractNum = document.querySelector('sup.sx-price-fractional').innerText; //Get the cents
                 let bookType = document.querySelector('div > div > div > div > div > div > div > a > h3[data-attribute]').innerText; //Get book type
                 let total = currency + price + '.' + fractNum;
-               
+
 
                 bookData.push({
                     book: {
                         title,
                         author,
                         total,
-                        bookType   
+                        bookType
                     }
-            
+
                 }); // Push an object with the data into our array
             }
             catch (err) {
                 console.error(err);
             }
         })
-         // Return book data array  
+        // Return book data array  
         return JSON.stringify(bookData, null, 2);
-
     });
 
 
     return searchBooks; // Return the result
     await browser.close();
+
 };
 
 crawlAmazon().then((response) => {
     console.log(response);
+    fs.writeFile("books.json", response, function(err) {
+
+ 
+        if (err) {
+          return console.log(err);
+        }
+      
+   
+        console.log("books.json was updated!");
+      
+      });
 });
-       
+
 
 
 
